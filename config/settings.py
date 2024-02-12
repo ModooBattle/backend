d680a508935eac28a541fa5752985b592367eb23
MiniMini-On <parkcy92@gmail.com>
2024-02-13 07:35:32 +0900

    :rocket: for deploy
    for deploy

--- config/settings.py
+++ config/settings.py
@@ -255,7 +255,7 @@ CORS_ALLOW_METHODS = [
     "POST",
     "PUT",
 ]
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
 # 커스텀유저
