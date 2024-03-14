280ed22b5591404377b547055e1a3e4d67f90c67
MiniMini-On <parkcy92@gmail.com>
2024-03-14 17:22:04 +0900

    :wrench: allow origin all 삭제 후 origin 추가
    allow origin all 삭제 후 origin 추가

--- config/settings.py
+++ config/settings.py
@@ -243,8 +243,8 @@ SIMPLE_JWT = {
 
 
 # CORS 관련 추가
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000", "https://modoobattle.com"]
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True  # 쿠키가 cross-site HTTP 요청에 포함될 수 있다
 CORS_ALLOW_METHODS = [
     "DELETE",
